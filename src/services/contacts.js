d697da391bbcc30780c1f205cc4fabdd8dc595a9
fatmanur-erdogan1805 <fatmanurerdogan1805@gmail.com>
2025-12-23 15:49:08 +0300

    Fix Contact model filename case for Linux

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import Contact from '../db/models/Contact.js';
+import {Contact } from '../db/models/Contact.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 import { SORT_ORDER } from '../constants/index.js';
 
